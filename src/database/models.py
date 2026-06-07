4887e0b9878db6a008b74b8688fb9091a1321d1d
Artem Terzi <111956601+ArtemTerzi@users.noreply.github.com>
2026-06-07 20:14:28 +0300

    Task 1-3

---
+++ src/database/models.py
@@ -0,0 +1,25 @@
+from datetime import datetime, date
+from sqlalchemy import Integer, String, func
+from sqlalchemy.orm import mapped_column, Mapped, DeclarativeBase
+from sqlalchemy.sql.sqltypes import DateTime, Date
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Contact(Base):
+    __tablename__ = "contacts"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    first_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str | None] = mapped_column(String(50))
+    phone_number: Mapped[str | None] = mapped_column(String(20), nullable=True)
+    birthday: Mapped[date | None] = mapped_column(Date, nullable=True)
+    created_at: Mapped[datetime] = mapped_column(
+        "created_at", DateTime, default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        "updated_at", DateTime, default=func.now(), onupdate=func.now()
+    )
